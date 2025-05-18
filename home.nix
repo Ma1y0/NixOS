3e74fd35fde8155f28cfb28c2b56f48168c38fcb
Matyáš Bár <matyas.barr@gmail.com>
2025-05-18 14:47:44 +0200

    dunst (notification deamon)

--- home.nix
+++ home.nix
@@ -36,6 +36,8 @@
     inputs.zen-browser.packages."${system}".twilight
   ];
 
+  services.dunst.enable = true;
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
