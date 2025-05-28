09bb5ad4f57d4c77f132adcf49f9eac296ad406a
Matyáš Bár <matyas.barr@gmail.com>
2025-05-28 10:39:44 +0200

    Random MAC

--- configuration.nix
+++ configuration.nix
@@ -24,6 +24,7 @@
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.wifi.macAddress = "random";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
